725041c2ffe9fc4c52db1d6d40ab0b02edabfd67
RootSec <itznaktuminajasiftw@gmail.com>
2019-12-24 14:12:38 +0100

    Update logout.php

--- core/pages/logout.php
+++ core/pages/logout.php
@@ -1,9 +1,8 @@
-<?php
-/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
- *   file                 :  logout.php
- * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
- *   author               :  Muhamed Skoko - mskoko.me@gmail.com
- * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+<?php 
+if(!defined('gp')) {
+   die('Direct access not permitted');
+}
+
 include_once($_SERVER['DOCUMENT_ROOT'].'/includes.php');
 // Get Current date, time
 $current_time = time();
@@ -12,6 +11,6 @@ $cookie_expiration_time = $current_time - (30 * 24 * 60 * 60);  // for 1 month
 setcookie('l0g1n', $_SESSION['UserLogin']['ID'], $cookie_expiration_time);
 session_unset();
 session_destroy();
-header('Location: /login?login');
+header('Location: /');
 die();
 ?>
