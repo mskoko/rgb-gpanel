12ae51456e1533387df0bba7cfb247d22c6cc482
RootSec <itznaktuminajasiftw@gmail.com>
2019-12-24 14:15:39 +0100

    Update alert.class.php

--- core/class/alert.class.php
+++ core/class/alert.class.php
@@ -1,9 +1,7 @@
 <?php
-/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
- *   file                 :  alert.class.php
- * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
- *   author               :  Muhamed Skoko - mskoko.me@gmail.com
- * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+if(!defined('gp')) {
+   die('Direct access not permitted');
+}
 
 class Alert {
 
